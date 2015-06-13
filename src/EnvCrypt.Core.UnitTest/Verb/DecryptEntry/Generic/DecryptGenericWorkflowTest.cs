9d66af63f4a81c2bb9565faad2eed092c06f50f5
lammichael <lammichael@users.noreply.github.com>
2015-06-14 00:32:42 +0100

    Added more unit tests for DecryptGenericWorkflow.

--- src/EnvCrypt.Core.UnitTest/Verb/DecryptEntry/Generic/DecryptGenericWorkflowTest.cs
+++ src/EnvCrypt.Core.UnitTest/Verb/DecryptEntry/Generic/DecryptGenericWorkflowTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using EnvCrypt.Core.EncrypedData.Poco;
 using EnvCrypt.Core.EncryptionAlgo;
 using EnvCrypt.Core.Key.Aes;
+using EnvCrypt.Core.Key.PlainText;
+using EnvCrypt.Core.Key.Rsa;
 using EnvCrypt.Core.Verb.DecryptEntry;
 using EnvCrypt.Core.Verb.DecryptEntry.Aes;
 using EnvCrypt.Core.Verb.DecryptEntry.Generic;
@@ -28,8 +31,6 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
             var aesEntryWorkflowBuilderMock = new Mock<IDecryptAesEntryWorkflowBuilder>(MockBehavior.Strict);
             var options = TwoDifferentAlgosExceptionTestSetup(out datLoaderMock);
 
-            
-
             // Act
             var workflow = new DecryptGenericWorkflow(datLoaderMock.Object, plainTextEntryWorkflowBuilderMock.Object, rsaEntryWorkflowBuilderMock.Object, aesEntryWorkflowBuilderMock.Object);
             Action act = () => workflow.Run(options);
@@ -44,8 +45,10 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
         {
             // Arrange
             Mock<IDatLoader<DatFromFileLoaderOptions>> datLoaderMock;
+
             var plainTextEntryWorkflowBuilderMock = new Mock<IDecryptPlainTextEntryWorkflowBuilder>(MockBehavior.Strict);
             var rsaEntryWorkflowBuilderMock = new Mock<IDecryptRsaEntryWorkflowBuilder>(MockBehavior.Strict);
+
             var aesEntryWorkflowBuilderMock = new Mock<IDecryptAesEntryWorkflowBuilder>(MockBehavior.Strict);
             aesEntryWorkflowBuilderMock.Setup(
                 b => b.WithDatLoader(It.IsAny<IDatLoader<DatFromFileLoaderOptions>>()))
@@ -53,6 +56,7 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
             aesEntryWorkflowBuilderMock.Setup(
                 b => b.Run(It.IsAny<DecryptEntryWorkflowOptions>()))
                 .Returns(new List<EntriesDecrypterResult<AesKey>>());
+
             var options = TwoDifferentAlgosExceptionTestSetup(out datLoaderMock);
 
             options.CategoryEntryPair.RemoveAt(0);
@@ -66,6 +70,119 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
         }
 
 
+        [Test]
+        public void Given_PlainTextDecryptionRequest_When_Run_Then_PlainTextWorkflowCalled()
+        {
+            // Arrange
+            Mock<IDatLoader<DatFromFileLoaderOptions>> datLoaderMock;
+
+            var rsaEntryWorkflowBuilderMock = new Mock<IDecryptRsaEntryWorkflowBuilder>(MockBehavior.Strict);
+            var aesEntryWorkflowBuilderMock = new Mock<IDecryptAesEntryWorkflowBuilder>(MockBehavior.Strict);
+
+            var plainTextEntryWorkflowBuilderMock = new Mock<IDecryptPlainTextEntryWorkflowBuilder>(MockBehavior.Strict);
+            plainTextEntryWorkflowBuilderMock.Setup(
+                b => b.WithDatLoader(It.IsAny<IDatLoader<DatFromFileLoaderOptions>>()))
+                .Returns(plainTextEntryWorkflowBuilderMock.Object);
+            plainTextEntryWorkflowBuilderMock.Setup(
+                b => b.Build())
+                .Returns(plainTextEntryWorkflowBuilderMock.Object);
+            plainTextEntryWorkflowBuilderMock.Setup(
+                b => b.Run(It.IsAny<DecryptPlainTextEntryWorkflowOptions>()))
+                .Returns(new List<EntriesDecrypterResult<PlainTextKey>>());
+
+            var options = TwoDifferentAlgosExceptionTestSetup(out datLoaderMock);
+
+            options.CategoryEntryPair = options.CategoryEntryPair.Where(p => p.Category == "dev").ToList();
+
+            // Act
+            var workflow = new DecryptGenericWorkflow(datLoaderMock.Object, plainTextEntryWorkflowBuilderMock.Object, rsaEntryWorkflowBuilderMock.Object, aesEntryWorkflowBuilderMock.Object);
+            var res = workflow.Run(options);
+
+            // Assert
+            plainTextEntryWorkflowBuilderMock.Verify(
+                b => b.Run(It.IsAny<DecryptPlainTextEntryWorkflowOptions>()), 
+                Times.Once);
+        }
+
+
+        [Test]
+        public void Given_RsaDecryptionRequest_When_Run_Then_RsaWorkflowCalled()
+        {
+            // Arrange
+            Mock<IDatLoader<DatFromFileLoaderOptions>> datLoaderMock;
+
+            var rsaEntryWorkflowBuilderMock = new Mock<IDecryptRsaEntryWorkflowBuilder>(MockBehavior.Strict);
+            var aesEntryWorkflowBuilderMock = new Mock<IDecryptAesEntryWorkflowBuilder>(MockBehavior.Strict);
+            var plainTextEntryWorkflowBuilderMock = new Mock<IDecryptPlainTextEntryWorkflowBuilder>(MockBehavior.Strict);
+
+            rsaEntryWorkflowBuilderMock.Setup(
+                b => b.WithDatLoader(It.IsAny<IDatLoader<DatFromFileLoaderOptions>>()))
+                .Returns(rsaEntryWorkflowBuilderMock.Object);
+            rsaEntryWorkflowBuilderMock.Setup(
+                b => b.Build())
+                .Returns(rsaEntryWorkflowBuilderMock.Object);
+            rsaEntryWorkflowBuilderMock.Setup(
+                b => b.Run(It.IsAny<DecryptEntryWorkflowOptions>()))
+                .Returns(new List<EntriesDecrypterResult<RsaKey>>());
+
+            var options = TwoDifferentAlgosExceptionTestSetup(out datLoaderMock);
+
+            options.CategoryEntryPair = options.CategoryEntryPair.Where(p => p.Category == "prod").ToList();
+
+            // Act
+            var workflow = new DecryptGenericWorkflow(
+                datLoaderMock.Object,
+                plainTextEntryWorkflowBuilderMock.Object,
+                rsaEntryWorkflowBuilderMock.Object,
+                aesEntryWorkflowBuilderMock.Object);
+            workflow.Run(options);
+
+            // Assert
+            rsaEntryWorkflowBuilderMock.Verify(
+                b => b.Run(It.IsAny<DecryptEntryWorkflowOptions>()),
+                Times.Once);
+        }
+
+
+        [Test]
+        public void Given_AesDecryptionRequest_When_Run_Then_AesWorkflowCalled()
+        {
+            // Arrange
+            Mock<IDatLoader<DatFromFileLoaderOptions>> datLoaderMock;
+
+            var rsaEntryWorkflowBuilderMock = new Mock<IDecryptRsaEntryWorkflowBuilder>(MockBehavior.Strict);
+            var aesEntryWorkflowBuilderMock = new Mock<IDecryptAesEntryWorkflowBuilder>(MockBehavior.Strict);
+            var plainTextEntryWorkflowBuilderMock = new Mock<IDecryptPlainTextEntryWorkflowBuilder>(MockBehavior.Strict);
+
+            aesEntryWorkflowBuilderMock.Setup(
+                b => b.WithDatLoader(It.IsAny<IDatLoader<DatFromFileLoaderOptions>>()))
+                .Returns(aesEntryWorkflowBuilderMock.Object);
+            aesEntryWorkflowBuilderMock.Setup(
+                b => b.Build())
+                .Returns(aesEntryWorkflowBuilderMock.Object);
+            aesEntryWorkflowBuilderMock.Setup(
+                b => b.Run(It.IsAny<DecryptEntryWorkflowOptions>()))
+                .Returns(new List<EntriesDecrypterResult<AesKey>>());
+
+            var options = TwoDifferentAlgosExceptionTestSetup(out datLoaderMock);
+
+            options.CategoryEntryPair = options.CategoryEntryPair.Where(p => p.Category == "uat").ToList();
+
+            // Act
+            var workflow = new DecryptGenericWorkflow(
+                datLoaderMock.Object,
+                plainTextEntryWorkflowBuilderMock.Object,
+                rsaEntryWorkflowBuilderMock.Object,
+                aesEntryWorkflowBuilderMock.Object);
+            workflow.Run(options);
+
+            // Assert
+            aesEntryWorkflowBuilderMock.Verify(
+                b => b.Run(It.IsAny<DecryptEntryWorkflowOptions>()),
+                Times.Once);
+        }
+
+
         private static DecryptGenericWorkflowOptions TwoDifferentAlgosExceptionTestSetup(out Mock<IDatLoader<DatFromFileLoaderOptions>> datLoaderMock)
         {
             var options = new DecryptGenericWorkflowOptions()
@@ -75,6 +192,7 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
                 {
                     new CategoryEntryPair("prod", "password"),
                     new CategoryEntryPair("uat", "password"),
+                    new CategoryEntryPair("dev", "username"),
                 }
             };
 
@@ -103,6 +221,18 @@ namespace EnvCrypt.Core.UnitTest.Verb.DecryptEntry.Generic
                             {
                                 Name = options.CategoryEntryPair[1].Entry,
                                 EncryptionAlgorithm = EnvCryptAlgoEnum.Aes
+                            }
+                        }
+                    },
+                    new Category()
+                    {
+                        Name = options.CategoryEntryPair[2].Category,
+                        Entries = new[]
+                        {
+                            new Entry()
+                            {
+                                Name = options.CategoryEntryPair[2].Entry,
+                                EncryptionAlgorithm = EnvCryptAlgoEnum.PlainText
                             }
                         }
                     }
