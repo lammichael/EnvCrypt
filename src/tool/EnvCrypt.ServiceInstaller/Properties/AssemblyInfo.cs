c47c03f9436a200d77a7c7b7395061d83c9bf088
lammichael <lammichael@users.noreply.github.com>
2015-06-14 00:55:54 +0100

    Added new project to install service that can run under an FID and will get password from DAT file.

---
+++ src/tool/EnvCrypt.ServiceInstaller/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnvCrypt.ServiceInstaller")]
+[assembly: AssemblyDescription("Installs a Windows Service to run under an FID")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("005f2003-4853-4291-baa4-5a374a45f970")]
